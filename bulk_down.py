85e6eb46f52aaa67cbe93de079b1fced7683d846
Prashant-1695 <prashant33968@gmail.com>
2025-01-15 19:05:49 +0530

    Move to Heaven
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>

--- bulk_down.py
+++ bulk_down.py
@@ -5,7 +5,7 @@ import time
 
 # Configuration
 DOWNLOADS_FOLDER = os.path.expanduser("./Downloads")
-SUB_FOLDER_NAME = "The Penthouse (2020)"  # Custom sub-folder name
+SUB_FOLDER_NAME = "Move to Heaven"  # Custom sub-folder name
 ARIA2_PATH = "aria2c"  # Ensure 'aria2c' is in your PATH
 LINKS_FILE = "links.txt"  # File containing URLs to download
 ZIP_ENABLED = True  # Set this to True or False based on your requirement
