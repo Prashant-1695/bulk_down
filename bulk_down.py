b6f95faf98369e757cc39d8433f407273798d900
Prashant-1695 <prashant33968@gmail.com>
2025-01-29 09:41:48 +0530

    Under the Queen's Umbrella
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>

--- bulk_down.py
+++ bulk_down.py
@@ -5,7 +5,7 @@ import time
 
 # Configuration
 DOWNLOADS_FOLDER = os.path.expanduser("./Downloads")
-SUB_FOLDER_NAME = "Fangs of Fortune"  # Custom sub-folder name
+SUB_FOLDER_NAME = "Under the Queen's Umbrella"  # Custom sub-folder name
 ARIA2_PATH = "aria2c"  # Ensure 'aria2c' is in your PATH
 LINKS_FILE = "links.txt"  # File containing URLs to download
 ZIP_ENABLED = True  # Set this to True or False based on your requirement
